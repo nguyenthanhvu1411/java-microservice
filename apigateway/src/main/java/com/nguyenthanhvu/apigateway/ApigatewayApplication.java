f6f19c1faa12fa5f746dee5b484ead7c57b0e252
ThanhVU <nthanhvu1411@gmail.com>
2026-03-05 08:49:37 +0700

    update

--- apigateway/src/main/java/com/nguyenthanhvu/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/nguyenthanhvu/apigateway/ApigatewayApplication.java
@@ -11,5 +11,5 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
-
+// API Gateway là một mẫu kiến trúc trong đó một dịch vụ trung gian (API Gateway) đứng giữa các client và các dịch vụ backend. API Gateway chịu trách nhiệm nhận tất cả các yêu cầu từ client, xử lý chúng và chuyển tiếp đến các dịch vụ backend tương ứng. Nó cũng có thể thực hiện các chức năng như xác thực, phân quyền, cân bằng tải, và quản lý lưu lượng.
 }
